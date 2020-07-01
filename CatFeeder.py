a8162e9cee3c65f812fffcf98a01ef2c1f2ce0ce
brettrowland4 <brettrowland4@gmail.com>
2020-06-30 20:17:35 -0400

    parent cdf528dabe67da22b95ff5ffa2bcbb93904a3b9a author brettrowland4 <brettrowland4@gmail.com> 1593562655 -0400 committer brettrowland4 <brettrowland4@gmail.com> 1593566553 -0400
    Created model for Owen and Wilson and changed script to output cat names
    
    removed santa model
    
    Added cat detection of Owen and Wilson
    
    added motor control and cat detection to catfeeder.py

--- CatFeeder.py
+++ CatFeeder.py
@@ -7,16 +7,51 @@ import imutils
 import time
 import cv2
 import os
+import RPi.GPIO as GPIO
 
 # define the paths to the Not Santa Keras deep learning model and
 # audio file
-MODEL_PATH = "santa_not_santa.model"
+MODEL_PATH = "cat_detector.model"
 # initialize the total number of frames that *consecutively* contain
 # santa along with threshold required to trigger the santa alarm
-TOTAL_CONSEC = 0
+TOTAL_CONSEC_WILSON = 0
+TOTAL_CONSEC_OWEN = 0
 TOTAL_THRESH = 20
 # initialize is the santa alarm has been triggered
 SANTA = False
+
+# initialize the servo
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+# configure GPIO17 for pwm with 50hz 
+p = GPIO.PWM(servoPIN, 50)
+
+# Test
+p.start(2.5)
+
+try:
+  while True:
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(12.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(10)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(7.5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(5)
+    time.sleep(0.5)
+    p.ChangeDutyCycle(2.5)
+    time.sleep(0.5)
+except KeyboardInterrupt:
+  p.stop()
+  GPIO.cleanup()
 
 # load the model
 print("[INFO] loading model...")
@@ -30,54 +65,51 @@ time.sleep(2.0)
 
 # loop over the frames from the video stream
 while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
 
     # prepare the image to be classified by our deep learning network
-	image = cv2.resize(frame, (28, 28))
-	image = image.astype("float") / 255.0
-	image = img_to_array(image)
-	image = np.expand_dims(image, axis=0)
-	# classify the input image and initialize the label and
-	# probability of the prediction
-	(notSanta, santa) = model.predict(image)[0]
-	label = "Not Santa"
-	proba = notSanta
+    image = cv2.resize(frame, (28, 28))
+    image = image.astype("float") / 255.0
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+    # classify the input image and initialize the label and
+    # probability of the prediction
+    (nothing, owen, wilson) = model.predict(image)[0]
+    label = "Nothing"
+    proba = nothing
 
     # check to see if santa was detected using our convolutional
-	# neural network
-	if santa > notSanta:
-		# update the label and prediction probability
-		label = "Santa"
-		proba = santa
-		# increment the total number of consecutive frames that
-		# contain santa
-		TOTAL_CONSEC += 1
-
-        # check to see if we should raise the santa alarm
-		if not SANTA and TOTAL_CONSEC >= TOTAL_THRESH:
-			# indicate that santa has been found
-			SANTA = True
-
-    # otherwise, reset the total number of consecutive frames and the
-	# santa alarm
-	else:
-		TOTAL_CONSEC = 0
-		SANTA = False
+    # neural network
+    if (wilson >= owen) and (wilson >= nothing):
+        label = "Wilson"
+        proba = wilson
+        TOTAL_CONSEC_WILSON += 1
+        TOTAL_CONSEC_OWEN = 0
+    elif (owen >= wilson) and (owen >= nothing):
+        label = "Owen"
+        proba = owen
+        TOTAL_CONSEC_WILSON = 0
+        TOTAL_CONSEC_OWEN += 1
+    else:
+        label = "Nothing"
+        proba = nothing
+        TOTAL_CONSEC_WILSON = 0;
+        TOTAL_CONSEC_OWEN = 0;
 
     # build the label and draw it on the frame
-	label = "{}: {:.2f}%".format(label, proba * 100)
-	frame = cv2.putText(frame, label, (10, 25),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
+    label = "{}: {:.2f}%".format(label, proba * 100)
+    frame = cv2.putText(frame, label, (10, 25),
+        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    # show the output frame
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
  
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
 # do a bit of cleanup
 print("[INFO] cleaning up...")
 cv2.destroyAllWindows()
